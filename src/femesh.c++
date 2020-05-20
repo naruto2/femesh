96e24b012efd60ae43d50be1ceb620686f66d0dd
Tsukuda Yoshio <tsukuda.yoshio@gmail.com>
2020-05-20 23:31:32 +0900

    delta, dimp2, in2xycを削除

--- src/femesh.c++
+++ src/femesh.c++
@@ -1,6 +1,11 @@
 #include "xmesh.hpp"
 
 
+extern void delaunay( vector<xyc>&, vector<nde>& );
+extern void sortmesh( vector<xyc>&, vector<nde>& );
+extern void p2( vector<xyc>&, vector<nde>& );
+
+
 void femesh(vector<xyc>&Z, vector<nde>&N)
 {
   delaunay(Z,N);
--- src/xmesh.hpp
+++ src/xmesh.hpp
@@ -1,8 +1,6 @@
-#ifndef _EST_XYC2MSH_HPP_
-#define _EST_XYC2MSH_HPP_
+#ifndef _FEMESH_HPP_
+#define _FEMESH_HPP_
 
-#include <iostream>
-#include <fstream>
 #include <vector>
 #include <cstdio>
 
@@ -11,15 +9,9 @@ using namespace std;
 typedef struct{ int a, b, c, A, B, C; } nde;
 typedef struct{ double x, y; char *label; } xyc;
 
-extern void in2xyc( istream &, vector<xyc>& );
-extern void delaunay( vector<xyc>&, vector<nde>& );
-extern void sortmesh( vector<xyc>&, vector<nde>& );
-extern void p2( vector<xyc>&, vector<nde>& );
-extern long dimp2( vector<nde>& );
-extern double delta(int, vector<xyc>&, vector<nde>&);
 extern void scanxyc( vector<xyc>& );
 extern void printxyc( vector<xyc>& );
 extern void femesh( vector<xyc>&, vector<nde>& );
 extern void plotmesh( vector<xyc>&, vector<nde>& );
-extern void printmesh(vector<xyc>&, vector<nde>& );
+extern void printmesh( vector<xyc>&, vector<nde>& );
 #endif
